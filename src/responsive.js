0413f1b9364349dd1f91f36d7b272cb7d008d109
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-08 13:49:23 +0200

    tablet option added

--- src/responsive.js
+++ src/responsive.js
@@ -4,10 +4,17 @@ export const mobile = (props) => {
   return css`
     @media only screen and (max-width: 375px) {
       ${props}
-    }
+    } 
   `;
 };
 export const tablet = (props) => {
+  return css`
+    @media only screen and (max-width: 700px) {
+      ${props}
+    }
+  `;
+};
+export const laptop = (props) => {
   return css`
     @media only screen and (max-width: 1440px) {
       ${props}
