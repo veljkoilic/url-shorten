bac7b7a2a3c81759204979245e2b22d314cc3b4c
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-09 21:21:30 +0200

    boost links section

--- src/components/BoostLinks.jsx
+++ src/components/BoostLinks.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import styled from "styled-components"
+import { PillButton } from "./PillButton";
 
 export const BoostLinks = () => {
   return (
-    <div>BoostLinks</div>
+    <Container>
+        <h2>Boost your links today</h2>
+        <Button>Get Started</Button>
+    </Container>
   )
 }
+
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 20px;
+background-color: var(--dark-violet);
+background-image: url('./images/bg-boost-desktop.svg');
+background-size: cover;
+background-repeat: no-repeat;
+h2{
+    color:#fff;
+    font-size: 30px;
+}
+
+`;
+const Button = styled(PillButton)``;
