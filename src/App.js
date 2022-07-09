301825861dfe7c1a918388ef5e2b5dc69c840dbd
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-09 10:10:19 +0200

    responsive hero

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <Header/>
       <Hero/>
+      <div>lol</div>
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { PillButton } from "./PillButton";
+import { mobile, tablet, laptop } from "../responsive";
 export const Hero = () => {
   return (
     <Container>
@@ -18,20 +19,47 @@ export const Hero = () => {
 
 const Container = styled.div`
   display: flex;
-  height: calc(100vh - 85px);
   padding-left: 10%;
   justify-content: flex-start;
   gap: 5%;
   background-color: #fff;
   padding-top: 100px;
+  padding-bottom: 100px;
+  ${laptop({
+    paddingLeft: "0",
+    flexDirection: "column-reverse",
+    alignItems: "center",
+  })}
+  ${mobile({
+    paddingTop: "10px",
+  })}
 `;
 const Left = styled.div`
   flex: 7;
+  ${laptop({
+    flex: "1",
+    width: "90%",
+    marginTop: "40px",
+    textAlign: "center",
+  })}
+  ${tablet({
+    width: "95%",
+  })}
+
   h1 {
     font-size: 100px;
     width: 100%;
     margin-bottom: 0;
     line-height: 120px;
+    margin-top: 0;
+    ${laptop({
+      fontSize: "70px",
+      lineHeight: "90px",
+    })}
+    ${mobile({
+      fontSize: "40px",
+      lineHeight: "45px",
+    })}
   }
   p {
     width: 80%;
@@ -39,12 +67,27 @@ const Left = styled.div`
     color: var(--grayish-violet);
     margin: 0px 0 50px 0;
     padding: 0;
+    ${laptop({
+      fontSize: "25px",
+      width: "100%",
+    })}
+    ${mobile({
+      fontSize: "20px",
+      width: "90%",
+      paddingLeft: "5%",
+      paddingRight: "5%",
+      paddingTop: "20px",
+      marginBottom: "20px"
+    })}
   }
 `;
 const Button = styled(PillButton)`
   padding: 20px 50px;
   font-size: 30px;
   border-radius: 50px;
+  ${mobile({
+    fontSize: "24px"
+  })}
 `;
 
 const Right = styled.div`
@@ -52,8 +95,18 @@ const Right = styled.div`
   display: flex;
   align-items: flex-start;
   margin-right: -10%;
+  max-width: 100%;
+  ${laptop({
+    flex: "1",
+  })}
 `;
 const Image = styled.img`
   width: 900px;
   padding-top: 0px;
+  ${laptop({
+    width: "800px",
+  })}
+  ${mobile({
+    width: "500px",
+  })}
 `;
